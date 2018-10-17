e7ff0d1f9b574080b9c8c8d902452dddecf639e5
blueskyxzy <blueskyxzy@gmail.com>
2018-10-17 15:31:13 +0800

    springboot多模块框架配置

--- springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/controller/TestController.java
+++ springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/controller/TestController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
