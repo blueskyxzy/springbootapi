3f6ffc94b93a08aae635dd633785743cd006b9fd
blueskyxzy <blueskyxzy@gmail.com>
2018-10-17 14:34:51 +0800

    springboot多模块框架配置

---
+++ springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/Application.java
@@ -0,0 +1,78 @@
+package com.xzy.springbootapi.admin;
+
+import org.apache.coyote.http11.AbstractHttp11Protocol;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import javax.servlet.MultipartConfigElement;
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	//Tomcat large file upload connection reset
+	//http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+	@Bean
+	public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		tomcat.addConnectorCustomizers((org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer) connector -> {
+			if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+				//-1 means unlimited
+				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+			}
+		});
+		return tomcat;
+	}
+
+	// Display all beans loaded by spring boot
+	@Bean
+	public CommandLineRunner run(ApplicationContext context) {
+		return args -> {
+			String[] beans = context.getBeanDefinitionNames();
+			Arrays.stream(beans).sorted().forEach(LOGGER::debug);
+		};
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("100MB");
+		factory.setMaxRequestSize("100MB");
+		return factory.createMultipartConfig();
+	}
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer(){
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.setSessionTimeout(1800);//单位为S
+			}
+		};
+	}
+}
