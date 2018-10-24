6dbdcd68c9668a876c55b673e5c3ed93c7250f84
blueskyxzy <blueskyxzy@gmail.com>
2018-10-24 11:30:42 +0800

    接口配置

--- springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/Application.java
+++ springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/Application.java
@@ -1,18 +1,83 @@
 package com.xzy.springbootapi.admin;
 
 
+import org.apache.coyote.http11.AbstractHttp11Protocol;
 import org.mybatis.spring.annotation.MapperScan;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
+import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.view.JstlView;
+import org.springframework.web.servlet.view.UrlBasedViewResolver;
+
+import javax.servlet.MultipartConfigElement;
+import java.util.Arrays;
 
 
 @SpringBootApplication
 @EnableTransactionManagement // 如果mybatis中service实现类中加入事务注解，需要此处添加该注解
 //@MapperScan(basePackages = "com.xzy.springbootapi.domain.mapper") // 扫描的是mapper.xml中namespace指向值的包位置
 public class Application{
+	private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	//Tomcat large file upload connection reset
+	//http://www.mkyong.com/spring/spring-file-upload-and-connection-reset-issue/
+	@Bean
+	public TomcatEmbeddedServletContainerFactory tomcatEmbedded() {
+		TomcatEmbeddedServletContainerFactory tomcat = new TomcatEmbeddedServletContainerFactory();
+		tomcat.addConnectorCustomizers((TomcatConnectorCustomizer) connector -> {
+			if ((connector.getProtocolHandler() instanceof AbstractHttp11Protocol<?>)) {
+				//-1 means unlimited
+				((AbstractHttp11Protocol<?>) connector.getProtocolHandler()).setMaxSwallowSize(-1);
+			}
+		});
+		return tomcat;
+	}
+
+	// Display all beans loaded by spring boot
+	@Bean
+	public CommandLineRunner run(ApplicationContext context) {
+		return args -> {
+			String[] beans = context.getBeanDefinitionNames();
+			Arrays.stream(beans).sorted().forEach(LOGGER::debug);
+		};
+	}
+
+	@Bean
+	public MultipartConfigElement multipartConfigElement() {
+		MultipartConfigFactory factory = new MultipartConfigFactory();
+		factory.setMaxFileSize("100MB");
+		factory.setMaxRequestSize("100MB");
+		return factory.createMultipartConfig();
+	}
+	@Bean
+	public UrlBasedViewResolver setupViewResolver() {
+		UrlBasedViewResolver resolver = new UrlBasedViewResolver();
+		resolver.setPrefix("/views/");
+		resolver.setSuffix(".jsp");
+		resolver.setViewClass(JstlView.class);
+		return resolver;
+	}
+	@Bean
+	public EmbeddedServletContainerCustomizer containerCustomizer(){
+		return new EmbeddedServletContainerCustomizer() {
+			@Override
+			public void customize(ConfigurableEmbeddedServletContainer container) {
+				container.setSessionTimeout(1800);//单位为S
+			}
+		};
 	}
 }
