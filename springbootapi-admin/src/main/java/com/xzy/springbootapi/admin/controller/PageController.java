7fc43889bc9b554befe1c804d93e1d2ee3f1891c
blueskyxzy <blueskyxzy@gmail.com>
2018-10-24 11:20:59 +0800

    接口测试

--- springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/controller/PageController.java
+++ springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/controller/PageController.java
@@ -1,8 +1,9 @@
-package com.xzy.springbootapi.admin;
+package com.xzy.springbootapi.admin.controller;
 
 import com.xzy.springbootapi.admin.controller.BaseRestCtrl;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -11,11 +12,11 @@ import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/p")
-public class PageController extends BaseRestCtrl{
+public class PageController{
 
     // 返回页面
-    @RequestMapping("/login")
-    public ModelAndView home(){
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public ModelAndView login() {
         ModelAndView mv = new ModelAndView();
         mv.setViewName("login");
         return mv;
