68f65c1ace963a54402c0e5135dc4870198f6919
blueskyxzy <blueskyxzy@gmail.com>
2018-10-26 16:15:52 +0800

    测试页面

---
+++ springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/config/WebConfig.java
@@ -0,0 +1,9 @@
+package com.xzy.springbootapi.admin.config;
+
+/**
+ * Created by xzy on 18/10/26  .
+ */
+
+public class WebConfig {
+
+}
--- springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/controller/PageController.java
+++ springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/controller/PageController.java
@@ -3,6 +3,7 @@ package com.xzy.springbootapi.admin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
