b744adbb11d46ef9d67071c505ab999803c3290e
blueskyxzy <blueskyxzy@gmail.com>
2018-10-31 16:17:03 +0800

    框架bug fix

--- springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/Application.java
+++ springbootapi-admin/src/main/java/com/xzy/springbootapi/admin/Application.java
@@ -1,26 +1,13 @@
 package com.xzy.springbootapi.admin;
 
-
-import org.apache.coyote.http11.AbstractHttp11Protocol;
-import org.mybatis.spring.annotation.MapperScan;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
-import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.context.embedded.tomcat.TomcatEmbeddedServletContainerFactory;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 import org.springframework.web.servlet.view.JstlView;
 import org.springframework.web.servlet.view.UrlBasedViewResolver;
-
-import javax.servlet.MultipartConfigElement;
-import java.util.Arrays;
 
 
 @SpringBootApplication
